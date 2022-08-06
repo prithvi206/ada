4f7190429de82fceb621df3dacd3cccf08d69bb8
prithvi206 <92168367+prithvi206@users.noreply.github.com>
2022-08-06 14:32:38 +0530

    Add files via upload

---
+++ gcdmodulo.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+    int m,n,rem,count=0,gcd;
+    printf("Enter 2 non-negative numbers\n");
+    scanf("%d %d",&m,&n);
+    while(n!=0)
+    {
+        rem=m%n;
+        count++;
+        m=n;
+        n=rem;
+    }
+    gcd=m;
+    printf("The gcd of the 2 numbers is %d\n",gcd);
+    printf("The count is %d\n",count);
+    return 0;
+}
